467b673f1b68f9d0a75e41e70bb259eb3f62f73b
Johan Book <{ID}+{username}@users.noreply.github.com>
2023-05-07 10:41:21 +0200

    feat(web-ui): show profile image and last message on match

--- services/web-ui/src/components/MatchListitem/MatchListItem.tsx
+++ services/web-ui/src/components/MatchListitem/MatchListItem.tsx
@@ -10,6 +10,7 @@ import {
 } from "@mui/material";
 
 import { MatchDetails } from "src/api";
+import { FALLBACK_IMAGES } from "src/constants/images";
 
 export interface MatchListItemProps {
   data: MatchDetails;
@@ -18,13 +19,15 @@ export interface MatchListItemProps {
 export function MatchListItem({
   data,
 }: MatchListItemProps): React.ReactElement {
+  const imageUrl = data.imageUrl || FALLBACK_IMAGES.PROFILE_IMAGE_URL;
+
   return (
     <ListItem divider>
       <ListItemButton component={Link} to={`/chat/${data.profileId}`}>
         <ListItemAvatar>
-          <Avatar />
+          <Avatar src={imageUrl} />
         </ListItemAvatar>
-        <ListItemText primary={data.name} />
+        <ListItemText primary={data.name} secondary={data.lastMessage} />
       </ListItemButton>
     </ListItem>
   );
