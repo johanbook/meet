bf2665460cec86dbec62df2fe9ec46f5b10f1301
Johan Book <{ID}+{username}@users.noreply.github.com>
2023-05-07 10:49:22 +0200

    refactor(api): fix formatting issue

--- services/api/src/application/matches/matches.module.ts
+++ services/api/src/application/matches/matches.module.ts
@@ -3,9 +3,9 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { Profile } from "src/infrastructure/database/entities/profile.entity";
 import { Match } from "src/infrastructure/database/views/matches.view";
+import { ObjectStorageService } from "src/infrastructure/objectStorage/object-storage.service";
 
 import { GetMatchesHandler } from "./queryHandlers/get-matches.handler";
-import {ObjectStorageService} from "src/infrastructure/objectStorage/object-storage.service";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Match, Profile])],
