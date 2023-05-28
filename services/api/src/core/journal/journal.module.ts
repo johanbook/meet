36538cd061f5efef4f47325b3ef66da88b0cdc18
Johan Book <{ID}+{username}@users.noreply.github.com>
2023-05-28 20:42:13 +0200

    fix(api): fix incorrect module import

--- services/api/src/core/journal/journal.module.ts
+++ services/api/src/core/journal/journal.module.ts
@@ -2,13 +2,19 @@ import { Module } from "@nestjs/common";
 import { CqrsModule } from "@nestjs/cqrs";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
+import { ProfileDomainModule } from "src/domain/profiles/profile-domain.module";
+
 import { CreateJournalEntryHandler } from "./application/handlers/command-handlers/create-journal-entry.handler";
 import { JournalController } from "./client/controllers/journal.controller";
 import { JournalEntry } from "./infrastructure/entities/journal-entry.entity";
 import { JournalLogger } from "./journal.listener";
 
 @Module({
-  imports: [CqrsModule, TypeOrmModule.forFeature([JournalEntry])],
+  imports: [
+    CqrsModule,
+    ProfileDomainModule,
+    TypeOrmModule.forFeature([JournalEntry]),
+  ],
   controllers: [JournalController],
   providers: [CreateJournalEntryHandler, JournalLogger],
 })
