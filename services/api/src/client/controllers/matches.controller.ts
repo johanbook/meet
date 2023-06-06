916737847197f7cd96a6ec6cc98d1491bf7caf87
Johan Book <{ID}+{username}@users.noreply.github.com>
2023-06-06 07:23:44 +0200

    feat(api): group matches  by messages

--- services/api/src/client/controllers/matches.controller.ts
+++ services/api/src/client/controllers/matches.controller.ts
@@ -3,7 +3,7 @@ import { QueryBus } from "@nestjs/cqrs";
 import { ApiTags } from "@nestjs/swagger";
 
 import { GetMatchesQuery } from "src/application/matches/contracts/get-matches.query";
-import { MatchDetails } from "src/application/matches/contracts/match.dto";
+import { AllMatchesDetails } from "src/application/matches/contracts/match.dto";
 
 @Controller("matches")
 @ApiTags("matches")
@@ -11,7 +11,7 @@ export class MatchesController {
   constructor(private queryBus: QueryBus) {}
 
   @Get()
-  async getMatches(): Promise<MatchDetails[]> {
+  async getMatches(): Promise<AllMatchesDetails> {
     const query = new GetMatchesQuery();
     return await this.queryBus.execute(query);
   }
