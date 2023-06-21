6d79e40145d1184d3a18b8bee543b5e1e913b5d6
Johan Book <{ID}+{username}@users.noreply.github.com>
2023-06-21 21:51:24 +0200

    test(web-ui): fix failing test

--- services/web-ui/src/components/ui/DatePicker/DatePicker.test.tsx
+++ services/web-ui/src/components/ui/DatePicker/DatePicker.test.tsx
@@ -1,11 +1,9 @@
-import { render, screen } from "src/test";
+import { render } from "src/test";
 
 import { DatePicker } from ".";
 
 describe("<DatePicker />", () => {
   it("renders", () => {
     render(<DatePicker onChange={jest.fn()} value={new Date()} />);
-    const text = screen.getByText(/my-text/);
-    expect(text).toBeInTheDocument();
   });
 });
