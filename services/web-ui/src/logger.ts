70fc0d0498ca9a061aaba15c0a3f00d99f4b4d2d
Johan Book <{ID}+{username}@users.noreply.github.com>
2023-06-24 19:07:49 +0200

    feat(web-ui): ship logs to trackiing service

--- services/web-ui/src/logger.ts
+++ services/web-ui/src/logger.ts
@@ -1,54 +1,84 @@
 /* eslint-disable no-console */
 /* eslint-disable sonarjs/no-identical-functions */
 
+type LogLevel = "debug" | "error" | "fatal" | "info" | "trace" | "warn";
+
+interface LogProps {
+  level: LogLevel;
+  msg: string;
+  props?: object;
+}
+
 export class Logger {
   constructor(private readonly name: string) {}
 
   debug(msg: string, props?: object) {
-    console.debug({
-      name: this.name,
-      msg,
-      ...props,
-    });
+    this.log({ level: "debug", msg, props });
   }
 
   error(msg: string, props?: object) {
-    console.error({
-      name: this.name,
-      msg,
-      ...props,
-    });
+    this.log({ level: "error", msg, props });
   }
 
   fatal(msg: string, props?: object) {
-    console.error({
-      name: this.name,
-      msg,
-      ...props,
-    });
+    this.log({ level: "fatal", msg, props });
   }
 
   info(msg: string, props?: object) {
-    console.info({
-      name: this.name,
-      msg,
-      ...props,
-    });
+    this.log({ level: "info", msg, props });
   }
 
   trace(msg: string, props?: object) {
-    console.trace({
-      name: this.name,
-      msg,
-      ...props,
-    });
+    this.log({ level: "trace", msg, props });
   }
 
   warn(msg: string, props?: object) {
-    console.warn({
-      name: this.name,
-      msg,
-      ...props,
+    this.log({ level: "warn", msg, props });
+  }
+
+  private log({ props, ...other }: LogProps): void {
+    const logProps = { ...other, props: { ...props, name: this.name } };
+
+    if (process.env.NODE_ENV === "development") {
+      this.consoleLog(logProps);
+    }
+
+    this.httpLog(logProps);
+  }
+
+  private consoleLog({ level, ...props }: LogProps): void {
+    switch (level) {
+      case "debug": {
+        console.debug(props);
+        return;
+      }
+      case "error":
+      case "fatal": {
+        console.error(props);
+        return;
+      }
+      case "info": {
+        console.info(props);
+        return;
+      }
+      case "trace": {
+        console.trace(props);
+        return;
+      }
+      case "warn": {
+        console.warn(props);
+        return;
+      }
+      default: {
+        throw new Error(`Unknown logging level ${level}`);
+      }
+    }
+  }
+
+  private async httpLog({ msg, props, level }: LogProps): Promise<void> {
+    await fetch("/tracking/log", {
+      body: JSON.stringify({ level, msg, props }),
+      method: "POST",
     });
   }
 }
