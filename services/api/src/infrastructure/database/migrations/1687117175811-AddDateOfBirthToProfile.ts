bb6592b4916a0c8b3aefc52303c576066500f01d
Johan Book <{ID}+{username}@users.noreply.github.com>
2023-06-24 21:31:07 +0200

    fix(api): fix typo in profile migration

--- services/api/src/infrastructure/database/migrations/1687117175811-AddDateOfBirthToProfile.ts
+++ services/api/src/infrastructure/database/migrations/1687117175811-AddDateOfBirthToProfile.ts
@@ -5,7 +5,7 @@ export class AddDateOfBirthToProfile1687117175811 implements MigrationInterface 
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "profile" ADD "dateOfBirth" date NOT NULL DEFAULT '1970-01-01'`);
-        await queryRunner.query(`ALTER TABLE "profile" ALTER COLUMN "dateOfBirth" date DROP DEFAULT`);
+        await queryRunner.query(`ALTER TABLE "profile" ALTER COLUMN "dateOfBirth" DROP DEFAULT`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
