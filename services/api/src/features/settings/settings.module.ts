31b3b331b472029b80c5c6db789efa3375bf3ef0
Johan Book <{ID}+{username}@users.noreply.github.com>
2023-07-18 09:16:31 +0200

    feat(api): create settings on creating profile

--- services/api/src/features/settings/settings.module.ts
+++ services/api/src/features/settings/settings.module.ts
@@ -2,7 +2,9 @@ import { Module } from "@nestjs/common";
 import { CqrsModule } from "@nestjs/cqrs";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
+import { CreateSettingsHandler } from "./application/handlers/command-handlers/create-settings.handler";
 import { UpdateSettingsHandler } from "./application/handlers/command-handlers/update-settings.handler";
+import { CreateSettingsOnProfileCreatedHandler } from "./application/handlers/event-handlers/create-settings-on-profile-created.handler";
 import { GetSettingsHandler } from "./application/handlers/query-handlers/get-settings.handler";
 import { SettingsController } from "./client/controllers/settings.controller";
 import { CurrentSettingsService } from "./domain/services/current-settings.service";
@@ -12,6 +14,8 @@ import { Settings } from "./infrastructure/entities/settings.entity";
   imports: [CqrsModule, TypeOrmModule.forFeature([Settings])],
   controllers: [SettingsController],
   providers: [
+    CreateSettingsHandler,
+    CreateSettingsOnProfileCreatedHandler,
     CurrentSettingsService,
     GetSettingsHandler,
     UpdateSettingsHandler,
