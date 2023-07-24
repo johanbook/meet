22acc095906a6a2269c1ff6b57c1201c41e33e18
Johan Book <{ID}+{username}@users.noreply.github.com>
2023-07-24 10:50:43 +0200

    feat(api): add method for validate and map data

--- services/api/src/core/mapper/mapper.spec.ts
+++ services/api/src/core/mapper/mapper.spec.ts
@@ -1,12 +1,13 @@
 import { Length } from "class-validator";
 
-import { map } from "./mapper";
+import { map, mapAndValidate } from "./mapper";
 
 describe(map.name, () => {
   it("maps properties", () => {
     class TestMyClass {
       myProperty!: string;
     }
+
     const mappedClass = map(TestMyClass, { myProperty: "my-string" });
     expect(mappedClass.myProperty).toBe("my-string");
   });
@@ -19,5 +20,31 @@ describe(map.name, () => {
 
     const mappedClass = map(TestMyClass, { myProperty: "my-string" });
     expect(mappedClass.myProperty).toBe("my-string");
+  });
+});
+
+describe(mapAndValidate.name, () => {
+  it("maps properties", async () => {
+    class TestMyClass {
+      myProperty!: string;
+    }
+
+    const mappedClass = await mapAndValidate(TestMyClass, {
+      myProperty: "my-string",
+    });
+    expect(mappedClass.myProperty).toBe("my-string");
+  });
+
+  it("throws on invalid validation", async () => {
+    class TestMyClass {
+      @Length(100, 200)
+      myProperty!: string;
+    }
+
+    expect(
+      mapAndValidate(TestMyClass, {
+        myProperty: "my-string",
+      }),
+    ).rejects.toBeTruthy();
   });
 });
--- services/api/src/core/mapper/mapper.ts
+++ services/api/src/core/mapper/mapper.ts
@@ -1,5 +1,7 @@
 import { ClassConstructor, plainToClass } from "class-transformer";
+import { validateOrReject } from "class-validator";
 
+/** Maps object array to class array **without** performing any validation */
 export function mapArray<T, V extends T, K>(
   cls: ClassConstructor<T>,
   data: K[],
@@ -11,6 +13,33 @@ export function mapArray<T, V extends T, K>(
   });
 }
 
+/** Maps object to class **without** performing any validation */
 export function map<T, V extends T>(cls: ClassConstructor<T>, plain: V): T {
   return plainToClass(cls, plain);
+}
+
+/** Maps object to class and performs validation */
+export async function mapAndValidate<T extends object, V extends T>(
+  cls: ClassConstructor<T>,
+  plain: V,
+): Promise<T> {
+  const value = map(cls, plain);
+
+  await validateOrReject(value);
+
+  return value;
+}
+
+/** Maps object array to class array and performs validation */
+export async function mapAndValidateArray<T extends object, V extends T, K>(
+  cls: ClassConstructor<T>,
+  data: K[],
+  transform: (item: K) => V,
+): Promise<T[]> {
+  const promises = data.map((item) => {
+    const transformed = transform(item);
+    return mapAndValidate(cls, transformed);
+  });
+
+  return await Promise.all(promises);
 }
