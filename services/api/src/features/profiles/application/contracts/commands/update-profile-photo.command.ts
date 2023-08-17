711b3524a912ad4a32a541619c55128dc2b32b0a
Johan Book <{ID}+{username}@users.noreply.github.com>
2023-08-17 22:35:37 +0200

    fix(api): set correct type on profile photo command

--- services/api/src/features/profiles/application/contracts/commands/update-profile-photo.command.ts
+++ services/api/src/features/profiles/application/contracts/commands/update-profile-photo.command.ts
@@ -1,10 +1,10 @@
 import { IsDefined } from "class-validator";
 
-import { BinaryFileArray } from "src/core/multipart";
+import { BinaryFile } from "src/core/multipart";
 import { IStorableObject } from "src/core/object-storage";
 
 export class UpdateProfilePhotoCommand {
   @IsDefined()
-  @BinaryFileArray()
+  @BinaryFile()
   photo!: IStorableObject;
 }
