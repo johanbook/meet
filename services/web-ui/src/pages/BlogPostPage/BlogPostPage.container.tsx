be343ef5e7686f0649cb72c8975f4ebbc2c4d1f1
Johan Book <{ID}+{username}@users.noreply.github.com>
2023-08-20 22:05:05 +0200

    fix(web-ui): add missing translation

--- services/web-ui/src/pages/BlogPostPage/BlogPostPage.container.tsx
+++ services/web-ui/src/pages/BlogPostPage/BlogPostPage.container.tsx
@@ -4,6 +4,7 @@ import { Box, Typography } from "@mui/material";
 
 import { blogsApi } from "src/apis";
 import { ErrorMessage } from "src/components/ui/ErrorMessage";
+import { useTranslation } from "src/core/i18n";
 import { CacheKeysConstants, useQuery } from "src/core/query";
 
 import { BlogPostPageComponent } from "./BlogPostPage.component";
@@ -12,6 +13,8 @@ import { BlogPostPageSkeleton } from "./BlogPostPage.skeleton";
 import { BlogPostForm } from "./components/BlogPostForm";
 
 export function BlogPostPageContainer(): React.ReactElement {
+  const { t } = useTranslation("blog");
+
   const { error, data, isLoading } = useQuery(
     CacheKeysConstants.BlogPosts,
     () => blogsApi.getBlogPosts()
@@ -45,7 +48,7 @@ export function BlogPostPageContainer(): React.ReactElement {
           <BlogPostForm />
         </Box>
 
-        <Typography>No blog posts found</Typography>
+        <Typography>{t("no-posts")}</Typography>
       </>
     );
   }
