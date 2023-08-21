f232a32c63e4603d0c3623508e1a1f54188b883d
Johan Book <{ID}+{username}@users.noreply.github.com>
2023-08-21 15:11:00 +0200

    feat(web-ui): add page for listing organizations

--- services/web-ui/src/pages/JournalPage/JournalPage.header.tsx
+++ services/web-ui/src/pages/JournalPage/JournalPage.header.tsx
@@ -5,7 +5,9 @@ import { Typography } from "@mui/material";
 export function JournalPageHeader(): React.ReactElement {
   return (
     <>
-      <Typography variant="h5">Journal</Typography>
+      <Typography sx={{ paddingTop: 2 }} variant="h5">
+        Journal
+      </Typography>
       <Typography color="textSecondary" gutterBottom>
         These are the actions that has been performed in the system by your
         account
