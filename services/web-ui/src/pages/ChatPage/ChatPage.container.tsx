9f15af9ec24ac90df8336d0e91ca75a32965ea14
Johan Book <{ID}+{username}@users.noreply.github.com>
2023-08-22 21:30:18 +0200

    fix(web-ui): fix translation typos

--- services/web-ui/src/pages/ChatPage/ChatPage.container.tsx
+++ services/web-ui/src/pages/ChatPage/ChatPage.container.tsx
@@ -8,6 +8,7 @@ import { chatsApi } from "src/apis";
 import { ChatMessageList } from "src/components/ChatMessageList";
 import { ChatTextField } from "src/components/ChatTextField";
 import { ErrorMessage } from "src/components/ui/ErrorMessage";
+import { useTranslation } from "src/core/i18n";
 import { NotificationEventsConstants } from "src/core/notifications";
 import { useHandleNotification } from "src/core/notifications";
 
@@ -17,6 +18,7 @@ import { ChatPageSkeleton } from "./ChatPage.skeleton";
 
 export function ChatPageContainer(): React.ReactElement {
   const { id } = useParams();
+  const { t } = useTranslation("chat");
 
   const { error, data, isLoading, refetch } = useQuery(`chat-${id}`, () =>
     chatsApi.getChats({ profileId: Number.parseInt(id || "") })
@@ -56,35 +58,6 @@ export function ChatPageContainer(): React.ReactElement {
 
   if (!data || data.length === 0) {
     return (
-      <ChatPageNav>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            height: "100%",
-          }}
-        >
-          <Box sx={{ flexGrow: 1 }}>
-            <Typography gutterBottom variant="h6">
-              No messages in chat
-            </Typography>
-
-            <Typography color="textSecondary">
-              Send a message and say 'hi'!
-            </Typography>
-          </Box>
-
-          <ChatTextField
-            onSentMessage={refetch}
-            receiverProfileId={receiverProfileId}
-          />
-        </Box>
-      </ChatPageNav>
-    );
-  }
-
-  return (
-    <ChatPageNav>
       <Box
         sx={{
           display: "flex",
@@ -92,6 +65,29 @@ export function ChatPageContainer(): React.ReactElement {
           height: "100%",
         }}
       >
+        <ChatPageNav>
+          <Box sx={{ flexGrow: 1 }}>
+            <Typography color="textSecondary">{t("no-messages")}</Typography>
+          </Box>
+
+          <ChatTextField
+            onSentMessage={refetch}
+            receiverProfileId={receiverProfileId}
+          />
+        </ChatPageNav>
+      </Box>
+    );
+  }
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100%",
+      }}
+    >
+      <ChatPageNav>
         <Box sx={{ flexGrow: 1 }}>
           <ChatMessageList messages={data} />
         </Box>
@@ -100,7 +96,7 @@ export function ChatPageContainer(): React.ReactElement {
           onSentMessage={refetch}
           receiverProfileId={receiverProfileId}
         />
-      </Box>
-    </ChatPageNav>
+      </ChatPageNav>
+    </Box>
   );
 }
