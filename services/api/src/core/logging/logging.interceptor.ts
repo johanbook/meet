008c87fa534f6f32c2d8d56a19a7f6ab276377a1
Johan Book <{ID}+{username}@users.noreply.github.com>
2023-08-26 09:07:56 +0200

    feat(api): print warning on long running requests

--- services/api/src/core/logging/logging.interceptor.ts
+++ services/api/src/core/logging/logging.interceptor.ts
@@ -9,22 +9,35 @@ import { tap } from "rxjs/operators";
 
 import { Logger } from "src/core/logging";
 
+const WARNING_DURATION_THRESHOLD_MS = 5000;
+
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
   private readonly logger = new Logger(LoggingInterceptor.name);
 
-  private logRequestDuration(durationInMs: number): void {
+  private logRequestDuration(durationInMs: number, request: Request): void {
     this.logger.debug({
       duration: durationInMs,
       msg: `Request took ${durationInMs}ms`,
     });
+
+    if (durationInMs > WARNING_DURATION_THRESHOLD_MS) {
+      this.logger.warn({
+        durationInMs,
+        msg: "Request took longer than usual",
+        method: request.method,
+        pathname: request.url,
+      });
+    }
   }
 
-  intercept(_: ExecutionContext, next: CallHandler): Observable<any> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const now = Date.now();
+    const http = context.switchToHttp();
+    const request = http.getRequest<Request>();
 
     return next
       .handle()
-      .pipe(tap(() => this.logRequestDuration(Date.now() - now)));
+      .pipe(tap(() => this.logRequestDuration(Date.now() - now, request)));
   }
 }
