1774544041beddb3db34e25e939982479782a181
Johan Book <{ID}+{username}@users.noreply.github.com>
2023-08-26 16:16:46 +0200

    feat(api): add email service

---
+++ services/api/src/core/email/domain/services/email.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@nestjs/common";
+import { createTransport, Transporter } from "nodemailer";
+
+import { emailConfig } from "../../email.config";
+
+interface SendEmailProps {
+  receivers: string[];
+  subject: string;
+  text: string;
+}
+
+@Injectable()
+export class EmailService {
+  private transporter: Transporter;
+
+  constructor() {
+    this.transporter = createTransport(emailConfig);
+  }
+
+  async sendEmail({ receivers, subject, text }: SendEmailProps) {
+    return await this.transporter.sendMail({
+      from: '"Meet" <no-replay@meetly.site>',
+      to: receivers.join(","),
+      subject,
+      text,
+    });
+  }
+}
