323ae6fed76b42b6d20c62615746826e5ee6f960
Johan Book <{ID}+{username}@users.noreply.github.com>
2023-09-02 20:50:11 +0200

    chore(repo): split auth service into dedicated api and ui

--- services/auth-api/src/main.ts
+++ services/auth-api/src/main.ts
@@ -1,12 +1,9 @@
-import { fastifyHttpProxy } from "@fastify/http-proxy";
-import { fastifyStatic } from "@fastify/static";
 import { NestFactory } from "@nestjs/core";
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from "@nestjs/platform-fastify";
 import { SwaggerModule } from "@nestjs/swagger";
-import * as path from "node:path";
 import { plugin } from "supertokens-node/framework/fastify/index.js";
 
 import { AppModule } from "./app.module";
@@ -15,10 +12,7 @@ import { SupertokensExceptionFilter } from "./core/supertokens/supertokens.filte
 import { createOpenApiDocument } from "./core/swagger/openapi";
 
 const PORT = Number.parseInt(process.env.PORT || "3000");
-const DEV_UI_URL = "http://localhost:8080";
 const PRODUCTION = process.env.NODE_ENV === "production";
-
-const PREFIX = "/login";
 
 async function bootstrap() {
   const nestLogger = new Logger("NestJS");
@@ -36,27 +30,10 @@ async function bootstrap() {
 
   app.register(plugin);
 
-  if (PRODUCTION) {
-    nestLogger.log("Using static file serving for web client");
-    app.register(fastifyStatic, {
-      prefix: PREFIX,
-      prefixAvoidTrailingSlash: true,
-      root: path.join(__dirname, "client"),
-      wildcard: true, // Needed for `prefixAvoidTrailingSlash` to work as intended
-    });
-  } else {
-    nestLogger.log("Using proxy for file serving for web client");
-    app.register(fastifyHttpProxy, {
-      upstream: DEV_UI_URL,
-      prefix: PREFIX,
-      rewritePrefix: PREFIX,
-    });
-  }
-
   app.useGlobalFilters(new SupertokensExceptionFilter());
 
   const document = createOpenApiDocument(app);
-  SwaggerModule.setup("/login/docs", app, document);
+  SwaggerModule.setup("/auth/api/docs", app, document);
 
   await app.listen(PORT, "0.0.0.0");
 }
