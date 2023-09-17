b2c1d885f01153d1d9224a50fbda487a06471d16
Johan Book <{ID}+{username}@users.noreply.github.com>
2023-09-17 21:08:34 +0200

    feat(web-ui): allow updating membership role

--- services/web-ui/src/pages/CurrentOrganizationPage/components/OrganizationMemberUpdateDialog/OrganizationMemberUpdateDialog.tsx
+++ services/web-ui/src/pages/CurrentOrganizationPage/components/OrganizationMemberUpdateDialog/OrganizationMemberUpdateDialog.tsx
@@ -1,12 +1,16 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
+import { useMutation, useQueryClient } from "react-query";
 
 import { Button, MenuItem, Select, Typography } from "@mui/material";
 
-import { OrganizationMemberDetails } from "src/api";
+import { OrganizationMemberDetails, UpdateMemberRoleCommand } from "src/api";
+import { organizationsApi } from "src/apis";
 import { Dialog } from "src/components/ui/Dialog";
 import { Role } from "src/core/authorization";
 import { GlobalDialogProps } from "src/core/dialog/dialog.context";
 import { useTranslation } from "src/core/i18n";
+import { CacheKeysConstants } from "src/core/query";
+import { useSnackbar } from "src/core/snackbar";
 
 export interface OrganizationMemberUpdateDialogProps extends GlobalDialogProps {
   member: OrganizationMemberDetails;
@@ -15,14 +19,43 @@ export interface OrganizationMemberUpdateDialogProps extends GlobalDialogProps {
 export function OrganizationMemberUpdateDialog({
   member,
 }: OrganizationMemberUpdateDialogProps): ReactElement {
+  const queryClient = useQueryClient();
+  const snackbar = useSnackbar();
   const { t } = useTranslation("organization");
+  const [role, setRole] = useState<Role>(member.role);
+
+  const mutation = useMutation(
+    (updateMemberRoleCommand: UpdateMemberRoleCommand) =>
+      organizationsApi.changeMemberRole({ updateMemberRoleCommand })
+  );
+
+  async function handleSave(onSuccess: () => void): Promise<void> {
+    await mutation.mutateAsync(
+      { id: member.id, role },
+      {
+        onError: () => snackbar.error(t("members.update.role.error")),
+        onSuccess: () => {
+          snackbar.success(t("members.update.role.success"));
+          queryClient.invalidateQueries([
+            CacheKeysConstants.CurrentOrganizationMembers,
+          ]);
+          onSuccess();
+        },
+      }
+    );
+  }
 
   return (
     <Dialog
       Actions={({ closeDialog }) => (
         <>
           <Button onClick={closeDialog}>Cancel</Button>
-          <Button>Save</Button>
+          <Button
+            disabled={role === member.role}
+            onClick={() => handleSave(closeDialog)}
+          >
+            Save
+          </Button>
         </>
       )}
       title={t("members.update.header") || ""}
@@ -32,7 +65,12 @@ export function OrganizationMemberUpdateDialog({
           {t("members.update.text")}
         </Typography>
 
-        <Select fullWidth label="Role" value={member.role}>
+        <Select
+          fullWidth
+          label="Role"
+          onChange={(event) => setRole(event.target.value as Role)}
+          value={role}
+        >
           {Object.entries(Role).map(([name, value]) => (
             <MenuItem key={value} value={value}>
               {name}
