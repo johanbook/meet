40a57fadf8c9a7ed6bd44949718b56ce66368a4c
Johan Book <13253042+johanbook@users.noreply.github.com>
2023-10-09 16:12:26 +0200

    feat(api): include profile details in blog post comment

--- services/api/src/features/blogs/application/contracts/dtos/blog-post-comment.dto.ts
+++ services/api/src/features/blogs/application/contracts/dtos/blog-post-comment.dto.ts
@@ -1,5 +1,8 @@
+import { BlogPostProfileDetails } from "./blog-post-profile.dto";
+
 export class BlogPostCommentDetails {
   id!: string;
   content!: string;
   createdAt!: string;
+  profile!: BlogPostProfileDetails;
 }
