0219c0a37aa2022a6bdee981c66aca868dba1f31
Johan Book <{ID}+{username}@users.noreply.github.com>
2023-10-20 16:28:06 +0200

    feat(web-ui): use spinner instead of skeleton for journal page

--- services/web-ui/src/pages/JournalPage/JournalPage.skeleton.tsx
+++ services/web-ui/src/pages/JournalPage/JournalPage.skeleton.tsx
@@ -1,16 +1,7 @@
 import React from "react";
 
-import { List, ListItem, Skeleton } from "@mui/material";
+import { LoadingView } from "src/views/LoadingView";
 
 export function JournalPageSkeleton(): React.ReactElement {
-  const data = [1, 2, 3, 4, 5];
-  return (
-    <List>
-      {data.map((index) => (
-        <ListItem key={index}>
-          <Skeleton height={30} width={400} />
-        </ListItem>
-      ))}
-    </List>
-  );
+  return <LoadingView />;
 }
