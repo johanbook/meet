bc054b079deed778c301494f65152a70e9c55497
Johan Book <{ID}+{username}@users.noreply.github.com>
2023-10-22 15:15:12 +0200

    feat(web-ui): add drawer component

---
+++ services/web-ui/src/components/Nav/desktopItems.tsx
@@ -0,0 +1,30 @@
+import {
+  AccountCircle,
+  Chat,
+  Feed,
+  SvgIconComponent,
+} from "@mui/icons-material";
+
+export interface NavItem {
+  Icon: SvgIconComponent;
+  name: string;
+  url: string;
+}
+
+export const desktopNavItems: NavItem[] = [
+  {
+    Icon: Feed,
+    name: "Blogs",
+    url: "/",
+  },
+  {
+    Icon: Chat,
+    name: "Conversations",
+    url: "/chat",
+  },
+  {
+    Icon: AccountCircle,
+    name: "Profile",
+    url: "/profile",
+  },
+];
---
+++ services/web-ui/src/components/ui/Drawer/MobileDrawer.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Box from "@mui/material/Box";
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
+import Toolbar from "@mui/material/Toolbar";
+
+interface DrawerProps {
+  children: React.ReactNode;
+}
+
+export function MobileDrawer({ children }: DrawerProps): React.ReactElement {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <>
+      <SwipeableDrawer
+        open={open}
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+      >
+        <Toolbar />
+        <Box sx={{ overflow: "auto" }}>{children}</Box>
+      </SwipeableDrawer>
+    </>
+  );
+}
