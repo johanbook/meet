4e21b69c62efcfe5e9dd11e5133fd88deb798893
Johan Book <13253042+johanbook@users.noreply.github.com>
2023-10-23 13:18:34 +0200

    feat(auth-ui): add translations to signup

---
+++ services/auth-ui/src/core/i18n/i18n.ts
@@ -0,0 +1,25 @@
+import { initReactI18next } from "react-i18next";
+
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Backend, { HttpBackendOptions } from "i18next-http-backend";
+
+export function registerI18n() {
+  i18n.use(Backend).init<HttpBackendOptions>({
+    backend: {
+      loadPath: "/login/locales/{{lng}}/{{ns}}.json",
+    },
+  });
+
+  i18n
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+      fallbackLng: "en",
+      debug: true,
+      interpolation: {
+        // Not needed for react as it escapes by default
+        escapeValue: false,
+      },
+    });
+}
