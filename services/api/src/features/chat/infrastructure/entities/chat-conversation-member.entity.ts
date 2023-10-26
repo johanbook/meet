285af41bc9ff213e52a1caa17cc09e4572061c47
Johan Book <13253042+johanbook@users.noreply.github.com>
2023-10-26 11:03:02 +0200

    feat(api): add chat conversation entity

---
+++ services/api/src/features/chat/infrastructure/entities/chat-conversation-member.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, ManyToOne, OneToOne } from "typeorm";
+
+import { BaseEntity } from "src/core/database";
+import { Profile } from "src/features/profiles";
+
+import { ChatConversation } from "./chat-conversation.entity";
+
+@Entity()
+export class ChatConversationMember extends BaseEntity {
+  @ManyToOne(() => ChatConversation, (conversation) => conversation.members)
+  conversation!: ChatConversation;
+
+  @OneToOne(() => Profile)
+  profile!: Profile;
+
+  @Column()
+  profileId!: number;
+}
