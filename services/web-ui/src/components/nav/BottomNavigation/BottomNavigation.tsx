0cb19c01aad44d691689fc567651fa5fc67e1765
Johan Book <{ID}+{username}@users.noreply.github.com>
2023-10-29 11:35:28 +0100

    refactor(web-ui): move nav items

--- services/web-ui/src/components/nav/BottomNavigation/BottomNavigation.tsx
+++ services/web-ui/src/components/nav/BottomNavigation/BottomNavigation.tsx
@@ -1,31 +1,19 @@
 import { ReactElement } from "react";
 import { Link as RouterLink, useLocation } from "react-router-dom";
 
-import { AccountCircle, Chat, Feed } from "@mui/icons-material";
 import {
-  Badge,
   BottomNavigationAction,
   BottomNavigation as MuiBottomNavigation,
   Paper,
 } from "@mui/material";
 
-import {
-  NotificationEventsConstants,
-  useNotifications,
-} from "src/core/notifications";
+import { mobileNavItems } from "../nav.items";
 
 export function BottomNavigation(): ReactElement {
-  const notifications = useNotifications();
   const location = useLocation();
 
   // Strips away ie chat id, meaning `/chat/12` -> `/chat`
-  let pathname = "/" + location.pathname.split("/")[1];
-
-  if (pathname === "/") {
-    pathname = "/blog";
-  }
-
-  const notificationData = notifications.data || {};
+  const pathname = "/" + location.pathname.split("/")[1];
 
   return (
     <Paper
@@ -34,35 +22,14 @@ export function BottomNavigation(): ReactElement {
       sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
     >
       <MuiBottomNavigation value={pathname}>
-        <BottomNavigationAction
-          component={RouterLink}
-          icon={
-            <Badge
-              color="error"
-              variant={
-                notificationData[NotificationEventsConstants.NEW_BLOG_POST]
-                  ? "dot"
-                  : undefined
-              }
-            >
-              <Feed />
-            </Badge>
-          }
-          to="/"
-          value="/blog"
-        />
-        <BottomNavigationAction
-          component={RouterLink}
-          icon={<Chat />}
-          to="/chat"
-          value="/chat"
-        />
-        <BottomNavigationAction
-          component={RouterLink}
-          icon={<AccountCircle />}
-          to="/profile"
-          value="/profile"
-        />
+        {mobileNavItems.map((item) => (
+          <BottomNavigationAction
+            component={RouterLink}
+            icon={<item.Icon />}
+            to={item.url}
+            value={item.url}
+          />
+        ))}
       </MuiBottomNavigation>
     </Paper>
   );
