1384b658db923994c5601911e64bf1629c8a0bd6
Johan Book <13253042+johanbook@users.noreply.github.com>
2023-12-06 19:08:36 +0800

    refactor(api): remove redundant code

--- services/api/src/features/blogs/application/contracts/commands/update-blog-post-reaction.command.ts
+++
@@ -1,9 +0,0 @@
-import { IsUUID, Length } from "class-validator";
-
-export class UpdateBlogPostReactionCommand {
-  @Length(1, 2048)
-  reaction!: string;
-
-  @IsUUID()
-  reactionId!: string;
-}
--- services/api/src/features/blogs/client/controllers/blog-reactions.controller.ts
+++ services/api/src/features/blogs/client/controllers/blog-reactions.controller.ts
@@ -1,10 +1,9 @@
-import { Body, Controller, Delete, Post, Put, Query } from "@nestjs/common";
+import { Body, Controller, Delete, Post, Query } from "@nestjs/common";
 import { CommandBus } from "@nestjs/cqrs";
 import { ApiTags } from "@nestjs/swagger";
 
 import { CreateBlogPostReactionCommand } from "../../application/contracts/commands/create-blog-post-reaction.command";
 import { DeleteBlogPostReactionCommand } from "../../application/contracts/commands/delete-blog-post-reaction.command";
-import { UpdateBlogPostReactionCommand } from "../../application/contracts/commands/update-blog-post-reaction.command";
 
 @Controller("blogs/reactions")
 @ApiTags("blogs")
@@ -14,13 +13,6 @@ export class BlogReactionsController {
   @Post()
   async createBlogPostReaction(
     @Body() command: CreateBlogPostReactionCommand,
-  ): Promise<null> {
-    return await this.commandBus.execute(command);
-  }
-
-  @Put()
-  async updateBlogPostReaction(
-    @Body() command: UpdateBlogPostReactionCommand,
   ): Promise<null> {
     return await this.commandBus.execute(command);
   }
