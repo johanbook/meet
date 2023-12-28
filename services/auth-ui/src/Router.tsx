5e23fc45ea1352bf1a4f554029189169b1244a88
Johan Book <13253042+johanbook@users.noreply.github.com>
2023-12-28 20:43:59 +0100

    feat(auth-ui): improve error message ui

--- services/auth-ui/src/Router.tsx
+++ services/auth-ui/src/Router.tsx
@@ -3,15 +3,21 @@ import { ReactElement } from "react";
 import { EmailPasswordPreBuiltUI } from "supertokens-auth-react/recipe/emailpassword/prebuiltui";
 import { canHandleRoute, getRoutingComponent } from "supertokens-auth-react/ui";
 
+import { ErrorView } from "src/views/ErrorView";
+
 export function Router(): ReactElement {
   if (!canHandleRoute([EmailPasswordPreBuiltUI])) {
-    return <p>An unexpected error has occurred: Unable to handle route</p>;
+    return (
+      <ErrorView message="An unexpected error has occurred: Unable to handle route" />
+    );
   }
 
   const component = getRoutingComponent([EmailPasswordPreBuiltUI]);
 
   if (!component) {
-    return <p>An unexpected error has occurred: Component not found</p>;
+    return (
+      <ErrorView message="An unexpected error has occurred: Component not found" />
+    );
   }
 
   return component;
