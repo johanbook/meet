556b6dd2269ea6216cf0976b39eee1f7e7fb645c
Johan Book <13253042+johanbook@users.noreply.github.com>
2023-12-28 22:01:27 +0100

    feat(web-ui): add quick switch button for changning organization

--- services/web-ui/src/components/ui/AppBar/AppBar.tsx
+++ services/web-ui/src/components/ui/AppBar/AppBar.tsx
@@ -1,17 +1,131 @@
-import React from "react";
+import { ReactElement } from "react";
 
-import { AppBar as MuiAppBar, Toolbar, Typography } from "@mui/material";
+import { ErrorOutline, KeyboardArrowDown } from "@mui/icons-material";
+import {
+  ButtonBase,
+  CircularProgress,
+  MenuItem,
+  AppBar as MuiAppBar,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 
+import { OrganizationDetails, SwitchOrganizationCommand } from "src/api";
 import { organizationsApi } from "src/apis";
+import { Menu } from "src/components/ui/Menu";
 import { config } from "src/config";
-import { CacheKeysConstants, useQuery } from "src/core/query";
+import { useTranslation } from "src/core/i18n";
+import {
+  CacheKeysConstants,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "src/core/query";
+import { useSnackbar } from "src/core/snackbar";
 
-export function AppBar(): React.ReactElement {
-  const { data } = useQuery({
+import { ErrorMessage } from "../ErrorMessage";
+
+interface MenuContentProps {
+  currentOrganizationId: number;
+}
+
+function MenuContent({ currentOrganizationId }: MenuContentProps) {
+  const query = useQuery({
+    queryKey: [CacheKeysConstants.OrganizationList],
+    queryFn: () => organizationsApi.getOrganizations(),
+  });
+
+  const queryClient = useQueryClient();
+
+  const { t } = useTranslation("organization-list");
+
+  const mutation = useMutation({
+    mutationFn: (switchOrganizationCommand: SwitchOrganizationCommand) =>
+      organizationsApi.switchOrganization({ switchOrganizationCommand }),
+  });
+
+  const snackbar = useSnackbar();
+
+  if (query.isPending) {
+    return <CircularProgress />;
+  }
+
+  if (query.error) {
+    return <ErrorMessage error={query.error} />;
+  }
+
+  async function handleClick(organization: OrganizationDetails): Promise<void> {
+    await mutation.mutateAsync(
+      { organizationId: organization.id },
+      {
+        onError: () => snackbar.error(t("actions.activate.error")),
+        onSuccess: () => {
+          queryClient.resetQueries();
+          snackbar.success(t("actions.activate.success"));
+        },
+      }
+    );
+  }
+
+  return (
+    <>
+      {query.data.map((organization) => (
+        <MenuItem
+          key={organization.id}
+          onClick={() => handleClick(organization)}
+          selected={organization.id === currentOrganizationId}
+        >
+          {organization.name}
+        </MenuItem>
+      ))}
+    </>
+  );
+}
+
+function AppBarContent(): ReactElement {
+  const query = useQuery({
     queryKey: [CacheKeysConstants.CurrentOrganization],
     queryFn: () => organizationsApi.getCurrentOrganization(),
   });
 
+  if (query.isPending) {
+    return (
+      <Typography color="primary" variant="h6">
+        {config.APP.NAME} <CircularProgress />
+      </Typography>
+    );
+  }
+
+  if (query.error) {
+    return (
+      <Typography color="primary" variant="h6">
+        {config.APP.NAME} <ErrorOutline />
+      </Typography>
+    );
+  }
+
+  return (
+    <Menu
+      Button={(props) => (
+        <ButtonBase>
+          <Typography
+            color="primary"
+            sx={{ display: "flex", alignItems: "center" }}
+            variant="h6"
+            {...props}
+          >
+            {config.APP.NAME} {query.data.name}
+            <KeyboardArrowDown />
+          </Typography>
+        </ButtonBase>
+      )}
+    >
+      <MenuContent currentOrganizationId={query.data.id} />
+    </Menu>
+  );
+}
+
+export function AppBar(): ReactElement {
   return (
     <MuiAppBar
       color="inherit"
@@ -19,9 +133,7 @@ export function AppBar(): React.ReactElement {
       sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
     >
       <Toolbar sx={{ display: "flex", justifyContent: "center" }}>
-        <Typography color="primary" variant="h6">
-          {config.APP.NAME} {data?.name}
-        </Typography>
+        <AppBarContent />
       </Toolbar>
     </MuiAppBar>
   );
