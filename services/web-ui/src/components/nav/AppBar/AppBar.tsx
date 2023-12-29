2ba55c3d91c4b0c3c2849d2d58366c9a3e6af56d
Johan Book <13253042+johanbook@users.noreply.github.com>
2023-12-29 16:31:45 +0100

    fix(web-ui): fix menu position (#694)

--- services/web-ui/src/components/nav/AppBar/AppBar.tsx
+++ services/web-ui/src/components/nav/AppBar/AppBar.tsx
@@ -105,6 +105,10 @@ function AppBarContent(): ReactElement {
 
   return (
     <Menu
+      anchorOrigin={{
+        horizontal: "right",
+        vertical: "bottom",
+      }}
       Button={(props) => (
         <ButtonBase>
           <Typography
