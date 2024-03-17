1397f85234e4fa484e5c4c6f8e7fd52690e8cde1
Johan Book <{ID}+{username}@users.noreply.github.com>
2024-03-17 16:48:53 +0100

    chore(homepage): migrate from react-scripts to vite

--- services/homepage/.eslintrc.js
+++ services/homepage/.eslintrc.js
@@ -5,13 +5,15 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
     "plugin:sonarjs/recommended",
     "plugin:unicorn/recommended",
-    "react-app",
-    "react-app/jest",
   ],
-  plugins: ["@typescript-eslint", "sonarjs", "unicorn"],
+  plugins: ["@typescript-eslint", "react", "react-hooks", "sonarjs", "unicorn"],
   rules: {
+    /** Not relevant in React v17 and later */
+    "react/react-in-jsx-scope": "off",
     "no-console": "error",
     "no-template-curly-in-string": "error",
     /** Empty interfaces are useful for later adding props */
