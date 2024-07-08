d5915bf22733f9b032a7626af01ea046035f02c6
Johan Book <{ID}+{username}@users.noreply.github.com>
2024-07-08 15:17:27 +0200

    feat(mobile-app): add anroid app

---
+++ services/mobile-app/app/src/main/java/meet/app/DelegationService.java
@@ -0,0 +1,14 @@
+package meet.app;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
