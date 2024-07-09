ecb1e571a51cdfd2ea90651b479dfc312feab141
Johan Book <{ID}+{username}@users.noreply.github.com>
2024-07-09 14:14:19 +0200

    feat(web-ui): remove redundant header

--- services/web-ui/src/pages/CurrentProfilePage/CurrentProfilePage.nav.tsx
+++ services/web-ui/src/pages/CurrentProfilePage/CurrentProfilePage.nav.tsx
@@ -1,8 +1,6 @@
-import React, { ReactNode } from "react";
+import { ReactElement, ReactNode } from "react";
 
-import { Typography } from "@mui/material";
-
-import { useTranslation } from "src/core/i18n";
+import { Box } from "@mui/material";
 
 interface CurrentProfilePageNavProps {
   children: ReactNode;
@@ -10,16 +8,6 @@ interface CurrentProfilePageNavProps {
 
 export function CurrentProfilePageNav({
   children,
-}: CurrentProfilePageNavProps): React.ReactElement {
-  const { t } = useTranslation("profile");
-
-  return (
-    <>
-      <Typography sx={{ paddingTop: 2 }} variant="h5">
-        {t("header")}
-      </Typography>
-
-      {children}
-    </>
-  );
+}: CurrentProfilePageNavProps): ReactElement {
+  return <Box sx={{ pt: 2 }}>{children}</Box>;
 }
