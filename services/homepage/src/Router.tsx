a9de616cf0b59543f95aa5cbdc2734e4620c8caf
Johan Book <{ID}+{username}@users.noreply.github.com>
2024-07-10 22:39:53 +0200

    fix(homepage): use hash router

--- services/homepage/src/Router.tsx
+++ services/homepage/src/Router.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 
 import Nav from "src/components/ui/Nav";
 
@@ -7,7 +7,7 @@ import { LandingPage } from "./pages/LandingPage";
 import { NotFoundPage } from "./pages/NotFoundPage/NotFoundPage";
 import { PrivacyPolicyPage } from "./pages/PrivacyPolicyPage/PrivacyPolicyPage";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Nav />,
