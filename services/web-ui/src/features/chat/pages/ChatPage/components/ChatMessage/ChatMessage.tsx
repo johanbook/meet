96926dbca7f3b331d61cdec7307c4db26443d638
Johan Book <13253042+johanbook@users.noreply.github.com>
2024-07-13 20:50:48 +0200

    feat(web-ui): redesign chat messages (#845)
    Co-authored-by: Johan Book <{ID}+{username}@users.noreply.github.com>

--- services/web-ui/src/features/chat/pages/ChatPage/components/ChatMessage/ChatMessage.tsx
+++ services/web-ui/src/features/chat/pages/ChatPage/components/ChatMessage/ChatMessage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { ListItem, ListItemText, useTheme } from "@mui/material";
+import { Box, Typography, useTheme } from "@mui/material";
 
 import { ChatMessageDetails } from "src/api";
 
@@ -12,19 +12,27 @@ export function ChatMessage({ message }: ChatMessageProps): React.ReactElement {
   const theme = useTheme();
 
   return (
-    <ListItem>
-      <ListItemText
-        primary={message.message}
-        primaryTypographyProps={{
-          align: message.sentByCurrentUser ? "right" : "left",
-        }}
+    <Box
+      component="li"
+      sx={{
+        py: 1,
+        display: "flex",
+        justifyContent: message.sentByCurrentUser ? "right" : "left",
+      }}
+    >
+      <Typography
+        component="span"
         sx={{
           background: theme.palette.background.paper,
           border: `1px solid ${theme.palette.divider} `,
           borderRadius: 3,
-          padding: 1,
+          maxWidth: "80%",
+          overflowWrap: "anywhere",
+          p: 1,
         }}
-      />
-    </ListItem>
+      >
+        {message.message}
+      </Typography>
+    </Box>
   );
 }
