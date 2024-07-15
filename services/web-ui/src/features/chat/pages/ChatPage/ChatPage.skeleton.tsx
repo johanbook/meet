26dc55265dffa8fb35a99e9dc6fc85251fcabb68
Johan Book <{ID}+{username}@users.noreply.github.com>
2024-07-15 22:40:34 +0200

    fix(web-ui): improve chat page styling

--- services/web-ui/src/features/chat/pages/ChatPage/ChatPage.skeleton.tsx
+++ services/web-ui/src/features/chat/pages/ChatPage/ChatPage.skeleton.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+import { ReactElement } from "react";
 
-import { Skeleton } from "@mui/material";
+import { Box, Skeleton } from "@mui/material";
 
-export function ChatPageSkeleton(): React.ReactElement {
+export function ChatPageSkeleton(): ReactElement {
+  const messages = [89, 168, 104, 67, 184, 110, 80];
   return (
     <>
-      <Skeleton />
+      {messages.map((width, index) => (
+        <Box
+          key={width}
+          sx={{
+            display: "flex",
+            justifyContent: index % 2 ? "right" : "left",
+          }}
+        >
+          <Skeleton
+            height={42}
+            sx={{
+              borderRadius: 3,
+              my: 1,
+            }}
+            width={width}
+            variant="rectangular"
+          />
+        </Box>
+      ))}
     </>
   );
 }
