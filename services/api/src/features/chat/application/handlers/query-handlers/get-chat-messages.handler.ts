98155c3e5cc096d1317d57756068946c01d8c10c
Johan Book <13253042+johanbook@users.noreply.github.com>
2024-07-15 18:14:55 +0200

    fix(api): order chat messages by creation (#862)
    Co-authored-by: Johan Book <{ID}+{username}@users.noreply.github.com>

--- services/api/src/features/chat/application/handlers/query-handlers/get-chat-messages.handler.ts
+++ services/api/src/features/chat/application/handlers/query-handlers/get-chat-messages.handler.ts
@@ -37,6 +37,9 @@ export class GetChatMessagesHandler
           profilePhoto: true,
         },
       },
+      order: {
+        created: "ASC",
+      },
       where: {
         conversation: {
           id: query.conversationId,
