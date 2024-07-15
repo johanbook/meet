e283929490ab5785899a0edd2fbf0b6ba70c9703
Johan Book <{ID}+{username}@users.noreply.github.com>
2024-07-15 21:53:02 +0200

    feat(api): include profiles in conversation respoinse

--- services/api/src/features/chat/application/contracts/dtos/chat-conversation.dto.ts
+++ services/api/src/features/chat/application/contracts/dtos/chat-conversation.dto.ts
@@ -1,7 +1,11 @@
+import { ChatMessageProfileDetails } from "./chat-message-profile.dto";
+
 export class ChatConversationDetails {
+  createdAt!: Date;
   id!: string;
   imageUrl?: string;
   lastMessage?: string;
   lastMessageSent?: Date;
   name!: string;
+  profiles!: ChatMessageProfileDetails[];
 }
