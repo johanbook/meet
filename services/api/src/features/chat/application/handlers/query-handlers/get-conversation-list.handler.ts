784114b483de12e03e61fd41c54b1f9ecf4a68a0
Johan Book <13253042+johanbook@users.noreply.github.com>
2024-07-15 18:54:06 +0200

    fix(api): show last message in conversation (#863)
    Co-authored-by: Johan Book <{ID}+{username}@users.noreply.github.com>

--- services/api/src/features/chat/application/handlers/query-handlers/get-conversation-list.handler.ts
+++ services/api/src/features/chat/application/handlers/query-handlers/get-conversation-list.handler.ts
@@ -1,11 +1,11 @@
 import { IQueryHandler, QueryHandler } from "@nestjs/cqrs";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
+import { In, Repository } from "typeorm";
 
 import { mapArray } from "src/core/mapper";
 import { CurrentOrganizationService } from "src/core/organizations";
 import { PhotoService } from "src/core/photos";
-import { CurrentProfileService } from "src/core/profiles";
+import { CurrentProfileService, Profile } from "src/core/profiles";
 
 import { ChatConversation } from "../../../infrastructure/entities/chat-conversation.entity";
 import { ChatConversationDetails } from "../../contracts/dtos/chat-conversation.dto";
@@ -20,6 +20,8 @@ export class GetConversationListHandler
     private readonly conversations: Repository<ChatConversation>,
     private readonly currentOrganizationService: CurrentOrganizationService,
     private readonly currentProfileService: CurrentProfileService,
+    @InjectRepository(Profile)
+    private readonly profiles: Repository<Profile>,
     private readonly photoService: PhotoService,
   ) {}
 
@@ -31,7 +33,13 @@ export class GetConversationListHandler
       await this.currentOrganizationService.fetchCurrentOrganizationId();
 
     const matchingConversations = await this.conversations.find({
+      order: {
+        messages: {
+          created: "DESC",
+        },
+      },
       relations: {
+        members: true,
         messages: true,
         photo: true,
       },
@@ -43,22 +51,66 @@ export class GetConversationListHandler
       },
     });
 
+    const profileIdSet = new Set<number>();
+
+    for (const conversation of matchingConversations) {
+      for (const member of conversation.members) {
+        profileIdSet.add(member.profileId);
+      }
+    }
+
+    const profileIds = [...profileIdSet];
+
+    // TODO: Combine these queries into one
+    const profileList = await this.profiles.find({
+      relations: {
+        profilePhoto: true,
+      },
+      where: {
+        id: In(profileIds),
+      },
+    });
+
+    const profileLookup: Record<string, Profile> = {};
+
+    for (const profile of profileList) {
+      profileLookup[profile.id] = profile;
+    }
+
     return mapArray(
       ChatConversationDetails,
       matchingConversations,
-      (conversation) => ({
-        id: conversation.id,
-        imageUrl:
-          conversation.photo &&
-          this.photoService.getUrl(
+      (conversation) => {
+        const lastMesage = conversation.messages[0];
+
+        let imageUrl: string | undefined;
+
+        if (conversation.photo) {
+          imageUrl = this.photoService.getUrl(
             conversation.photo,
             "chat-conversation-photo",
-          ),
-        // TODO: Fix this
-        lastMessage: "", //conversation.lastMessage,
-        lastMessageSent: new Date(), //conversation.lastMessageSent,
-        name: conversation.name,
-      }),
+          );
+        }
+
+        if (conversation.members.length === 1) {
+          const profile = profileLookup[conversation.members[0].profileId];
+          imageUrl =
+            profile.profilePhoto &&
+            this.photoService.getUrl(profile.profilePhoto, "profile-photo");
+        }
+
+        return {
+          id: conversation.id,
+          imageUrl,
+          lastMessage: lastMesage ? lastMesage.message : undefined,
+          lastMessageSent: lastMesage ? lastMesage.created : undefined,
+          name:
+            conversation.name ||
+            conversation.members
+              .map((x) => profileLookup[x.profileId].name)
+              .join(", "),
+        };
+      },
     );
   }
 }
