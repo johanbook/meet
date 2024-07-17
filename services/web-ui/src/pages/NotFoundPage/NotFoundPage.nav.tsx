9922af3b224fc4a1c5d0e35c30b9d2d1d0e3286c
Johan Book <{ID}+{username}@users.noreply.github.com>
2024-07-17 22:21:22 +0200

    refactor(web-ui): use shared nav on not found page

--- services/web-ui/src/pages/NotFoundPage/NotFoundPage.nav.tsx
+++ services/web-ui/src/pages/NotFoundPage/NotFoundPage.nav.tsx
@@ -1,26 +1,11 @@
 import { ReactElement, ReactNode } from "react";
 
-import { Box, Toolbar } from "@mui/material";
-
-import { AppBar } from "src/components/nav/AppBar";
-import { BottomNavigation } from "src/components/nav/BottomNavigation";
+import { Nav } from "src/components/nav";
 
 export interface NotFoundNavProps {
   children: ReactNode;
 }
 
 export function NotFoundNav({ children }: NotFoundNavProps): ReactElement {
-  return (
-    <Box sx={{ display: "flex", height: "100vh" }}>
-      <AppBar />
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, padding: 3, paddingTop: 1, height: "100%" }}
-      >
-        {children}
-        <Toolbar />
-      </Box>
-      <BottomNavigation />
-    </Box>
-  );
+  return <Nav>{children}</Nav>;
 }
