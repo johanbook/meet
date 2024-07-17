d5dbaeb86d872890ad3ad9ef263ae1e7dc7eeea9
Johan Book <{ID}+{username}@users.noreply.github.com>
2024-07-17 17:07:06 +0200

    chore(web-ui): correctly cache s3 images

--- services/web-ui/src/serviceWorker.js
+++ services/web-ui/src/serviceWorker.js
@@ -1,11 +1,26 @@
+/* eslint-disable unicorn/no-abusive-eslint-disable */
+/* eslint-disable */
 importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js"
+  "https://storage.googleapis.com/workbox-cdn/releases/7.1.0/workbox-sw.js"
 );
 
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+// This must come before any other workbox.* methods.
+workbox.setConfig({
+  debug: true,
+});
+
+// workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
 // Images
 workbox.routing.registerRoute(
-  new RegExp(".(png|svg|jpg|jpeg)$"),
-  workbox.strategies.cacheFirst()
+  new RegExp("s3."),
+  workbox.strategies.cacheFirst({
+    cacheName: "images-cache",
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 50, // Limit number of images cached
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
 );
