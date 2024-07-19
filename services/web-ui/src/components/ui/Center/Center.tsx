7de78c200e67e365d58e198b491fd0a4e06ec214
Johan Book <13253042+johanbook@users.noreply.github.com>
2024-07-19 08:40:46 +0200

    refactor(web-ui): clean up import

--- services/web-ui/src/components/ui/Center/Center.tsx
+++ services/web-ui/src/components/ui/Center/Center.tsx
@@ -1,12 +1,12 @@
-import React from "react";
+import { ReactElement, ReactNode } from "react";
 
 import { Box } from "@mui/material";
 
-export interface CenterProps {
-  children: React.ReactNode;
+interface CenterProps {
+  children: ReactNode;
 }
 
-export function Center({ children }: CenterProps): React.ReactElement {
+export function Center({ children }: CenterProps): ReactElement {
   return (
     <Box sx={{ display: "flex", justifyContent: "center" }}> {children}</Box>
   );
