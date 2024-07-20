6c312a953ef82eb8a3b9f3960c5910b7edd74765
Johan Book <{ID}+{username}@users.noreply.github.com>
2024-07-20 19:27:11 +0200

    feat(web-ui): improve skeleton for photos page

--- services/web-ui/src/features/blogs/pages/BlogPhotoListPage/BlogPhotoListPage.skeleton.tsx
+++ services/web-ui/src/features/blogs/pages/BlogPhotoListPage/BlogPhotoListPage.skeleton.tsx
@@ -1,11 +1,38 @@
 import { ReactElement } from "react";
 
-import { Skeleton } from "@mui/material";
+import { Box, Skeleton } from "@mui/material";
+
+const getNumElement = (num: number) => {
+  const arr: number[] = [];
+
+  for (let x = 0; x < num; x++) {
+    arr.push(x);
+  }
+
+  return arr;
+};
 
 export function BlogPhotoListPageSkeleton(): ReactElement {
+  const photos = [3, 1, 4, 2];
+
   return (
     <>
-      <Skeleton />
+      {photos.map((photo, index) => (
+        <Box key={index} sx={{ pt: 3, px: 1 }}>
+          <Skeleton />
+
+          <Box sx={{ display: "flex", gap: 1, pt: 1 }}>
+            {getNumElement(photo).map((v) => (
+              <Skeleton
+                key={index + "" + v}
+                sx={{ height: 100, width: 100 }}
+                variant="rectangular"
+              />
+            ))}
+            <Skeleton variant="rectangular" />
+          </Box>
+        </Box>
+      ))}
     </>
   );
 }
