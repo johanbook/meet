43d8941f6c42473bf5e246bca8adaf43daa7a5e1
Johan Book <{ID}+{username}@users.noreply.github.com>
2024-07-20 10:59:27 +0200

    feat(api): enable web push notifications

--- services/api/src/core/notifications/client/gateways/notification.web-push.gateway.ts
+++ services/api/src/core/notifications/client/gateways/notification.web-push.gateway.ts
@@ -18,14 +18,35 @@ export class NotificationWebPushGateway {
     );
   }
 
-  public async sendWebPush(profileId: number, notification: INotification) {
+  private async sendWebPushToProfile(
+    profileId: number,
+    notification: INotification,
+  ): Promise<boolean> {
     const pushSubscription =
       this.notificationSubscriptionService.getSubscription(profileId);
 
     if (!pushSubscription) {
-      return;
+      return false;
     }
 
-    await sendNotification(pushSubscription, JSON.stringify(notification));
+    const result = await sendNotification(
+      pushSubscription,
+      JSON.stringify(notification),
+    );
+
+    return result.statusCode < 400;
+  }
+
+  public async sendWebPush(
+    profileIds: number[],
+    notification: INotification,
+  ): Promise<Record<number, boolean>> {
+    const result: Record<number, boolean> = {};
+
+    for (const profileId of profileIds) {
+      await this.sendWebPushToProfile(profileId, notification);
+    }
+
+    return result;
   }
 }
