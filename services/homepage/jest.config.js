daef30952b969f2a7b67152e332ef20877ce519d
Johan Book <13253042+johanbook@users.noreply.github.com>
2025-04-21 10:12:12 +0200

    test(homepage): stop reporting coverage

--- services/homepage/jest.config.js
+++ services/homepage/jest.config.js
@@ -1,14 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   collectCoverageFrom: ["src/**/*.{ts,tsx}"],
-  coverageThreshold: {
-    global: {
-      branches: 2,
-      functions: 6,
-      lines: 22,
-      statements: 25,
-    },
-  },
   moduleDirectories: ["node_modules", __dirname],
   preset: "ts-jest",
   setupFilesAfterEnv: ["./src/setupTests.ts"],
