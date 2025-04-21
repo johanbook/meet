68002d4be86d0e4906755b933b5bbe597b3ab9c2
Johan Book <13253042+johanbook@users.noreply.github.com>
2025-04-21 11:32:35 +0200

    chore(auth-ui): fix incorrect path (#1092)

--- services/auth-ui/vite.config.ts
+++ services/auth-ui/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
+  base: "/login",
   resolve: {
     alias: {
       src: fileURLToPath(new URL("./src", import.meta.url)),
