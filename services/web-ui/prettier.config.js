44e760c17b56556f82813ef08b2bd455e8316631
Johan Book <13253042+johanbook@users.noreply.github.com>
2025-04-25 20:49:23 +0200

    chore(web-ui): use plugin sort in prettier config

--- services/web-ui/prettier.config.js
+++ services/web-ui/prettier.config.js
@@ -2,4 +2,5 @@ module.exports = {
   importOrder: ["^react", "^(?!src|\\.)", "^src", "^\\."],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
+  plugins: ["@trivago/prettier-plugin-sort-imports"],
 };
