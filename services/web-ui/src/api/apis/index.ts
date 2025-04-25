e87fb50ed770cfb2401a88295986f36de8bb8e58
Johan Book <13253042+johanbook@users.noreply.github.com>
2025-04-25 15:50:14 +0200

    refactor(web-ui): include api defintions in source code (#1121)

---
+++ services/web-ui/src/api/apis/index.ts
@@ -0,0 +1,12 @@
+// @ts-nocheck
+/* tslint:disable */
+/* eslint-disable */
+export * from './BlogsApi';
+export * from './ChatsApi';
+export * from './ClassificationsApi';
+export * from './HealthApi';
+export * from './JournalApi';
+export * from './NotificationsApi';
+export * from './OrganizationsApi';
+export * from './ProfileApi';
+export * from './SettingsApi';
