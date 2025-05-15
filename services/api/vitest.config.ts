cbf100fc4a533f9aaa19e49322c3cebc31aaa369
Johan Book <13253042+johanbook@users.noreply.github.com>
2025-05-15 08:38:58 +0200

    chore(api): add swc to vitest

--- services/api/vitest.config.ts
+++ services/api/vitest.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vitest/config";
+import swc from 'unplugin-swc';
 
 export default defineConfig({
+  plugins: [
+    swc.vite()
+  ],
   test: {
     coverage: {
       thresholds: {
