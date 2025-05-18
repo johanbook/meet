d3faca58de808b5296bbf1e6a1f555c1a9b65566
Johan Book <13253042+johanbook@users.noreply.github.com>
2025-05-18 14:50:05 +0200

    chore(repo): remove tracking service (#1290)

--- services/tracking/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-
-import { TrackingModule } from "./features/tracking/tracking.module";
-
-@Module({
-  imports: [TrackingModule],
-  providers: [],
-})
-export class AppModule {}
