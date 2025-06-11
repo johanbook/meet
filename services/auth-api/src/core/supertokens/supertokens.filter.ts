dfe6453f2828102ab2759f11450c0cd2104821ab
Johan Book <13253042+johanbook@users.noreply.github.com>
2025-06-11 15:27:58 +0200

    refactor(auth-api): improve types

--- services/auth-api/src/core/supertokens/supertokens.filter.ts
+++ services/auth-api/src/core/supertokens/supertokens.filter.ts
@@ -1,6 +1,5 @@
 import { ArgumentsHost, Catch, ExceptionFilter } from "@nestjs/common";
 import { ErrorHandler } from "@nestjs/common/interfaces";
-import { NextFunction, Request, Response } from "express";
 import { Error as STError } from "supertokens-node";
 import { errorHandler } from "supertokens-node/framework/fastify";
 
@@ -15,16 +14,11 @@ export class SupertokensExceptionFilter implements ExceptionFilter {
   catch(exception: Error, host: ArgumentsHost) {
     const context = host.switchToHttp();
 
-    const resp = context.getResponse<Response>();
+    const resp = context.getResponse();
     if (resp.headersSent) {
       return;
     }
 
-    this.handler(
-      exception,
-      context.getRequest<Request>(),
-      resp,
-      context.getNext<NextFunction>(),
-    );
+    this.handler(exception, context.getRequest(), resp, context.getNext());
   }
 }
