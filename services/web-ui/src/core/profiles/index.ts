f72d121c7aa2f35e716f5295d40ef8080bd34e1e
Johan Book <13253042+johanbook@users.noreply.github.com>
2025-06-18 20:49:23 +0200

    refactor(web-ui): remove comments

--- services/web-ui/src/core/profiles/index.ts
+++ services/web-ui/src/core/profiles/index.ts
@@ -1,7 +1,4 @@
-// Guards
 export { ProfileGuard } from "./guards";
-
-// Pages
 export { CurrentProfilePage } from "./pages/CurrentProfilePage";
 export { ProfileCreationPage } from "./pages/ProfileCreationPage";
 export { ProfilePage } from "./pages/ProfilePage";
