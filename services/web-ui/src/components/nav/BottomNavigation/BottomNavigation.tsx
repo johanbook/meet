31f24dae45829a99e7ec726ef719588722b7d896
Johan Book <13253042+johanbook@users.noreply.github.com>
2025-06-20 08:06:10 +0200

    feat(web-ui): update mobile nav icon size and nav background (#1444)
    * feat(web-ui): update mobile nav icon sizes
    
    * refactor(web-ui): use bottom nav

--- services/web-ui/src/components/nav/BottomNavigation/BottomNavigation.tsx
+++ services/web-ui/src/components/nav/BottomNavigation/BottomNavigation.tsx
@@ -1,66 +1,48 @@
 import { ReactElement } from "react";
-import { Link as RouterLink, useLocation } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 
-import { AppBar, Button, Toolbar } from "@mui/material";
+import {
+  BottomNavigationAction,
+  Box,
+  BottomNavigation as MuiBottomNavigation,
+} from "@mui/material";
 
 import { mobileNav } from "../nav.items";
-import { NavItem } from "../types";
-
-interface BottomNavigationListItemProps {
-  fractionalWidth: number;
-  item: NavItem;
-}
-
-function BottomNavigationListItem({
-  fractionalWidth,
-  item,
-}: BottomNavigationListItemProps): ReactElement {
-  const location = useLocation();
-  const isActive = item.checkIfActive(location.pathname);
-
-  return (
-    <Button
-      component={RouterLink}
-      to={item.url}
-      sx={{ width: `${100 * fractionalWidth}%` }}
-    >
-      <item.Icon
-        sx={{
-          color: isActive ? "primary.main" : "action.active",
-        }}
-      />
-    </Button>
-  );
-}
 
 export function BottomNavigation(): ReactElement {
-  const fractionalWidth = 1 / mobileNav.bottom.length;
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const item = mobileNav.bottom.find((item) =>
+    item.checkIfActive(location.pathname),
+  );
 
   return (
-    <AppBar
-      color="inherit"
+    <Box
       component="nav"
-      position="fixed"
       sx={{
+        position: "fixed",
         top: "auto",
         bottom: 0,
         left: 0,
         right: 0,
 
         // This looks better on mobile as there will be navigation buttons below
-        boxShadow: "none",
         borderTop: (theme) => `1px solid ${theme.palette.divider}`,
       }}
     >
-      <Toolbar>
+      <MuiBottomNavigation
+        onChange={(_, path) => navigate(path)}
+        value={item?.url}
+      >
         {mobileNav.bottom.map((item) => (
-          <BottomNavigationListItem
-            fractionalWidth={fractionalWidth}
+          <BottomNavigationAction
             key={item.url}
-            item={item}
+            icon={<item.Icon />}
+            value={item.url}
           />
         ))}
-      </Toolbar>
-    </AppBar>
+      </MuiBottomNavigation>
+    </Box>
   );
 }
