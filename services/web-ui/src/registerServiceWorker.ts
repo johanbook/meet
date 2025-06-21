aef5501dc37151f8908bbc4b48e538c39d17e163
Johan Book <13253042+johanbook@users.noreply.github.com>
2025-06-21 17:39:16 +0200

    fix(web-ui): avoid subscribing to notifications when opening app (#1451)
    Co-authored-by: Johan Book <{ID}+{username}@users.noreply.github.com>

--- services/web-ui/src/registerServiceWorker.ts
+++ services/web-ui/src/registerServiceWorker.ts
@@ -6,17 +6,10 @@ export async function registerServiceWorker() {
   const registration =
     await navigator.serviceWorker.register(SERVICE_WORKER_PATH);
 
-  let subscription = await registration.pushManager.getSubscription();
+  const subscription = await registration.pushManager.getSubscription();
 
   if (!subscription) {
-    await Notification.requestPermission();
-
-    const vapid = await notificationsApi.getVapid();
-
-    subscription = await registration.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey: vapid.key,
-    });
+    return;
   }
 
   const saveNotificationSubscriptionCommand = subscription.toJSON();
