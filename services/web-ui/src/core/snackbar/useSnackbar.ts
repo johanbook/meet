fbc17546de0576e153a99d1cc7c89cc0ee7e9108
Johan Book <13253042+johanbook@users.noreply.github.com>
2025-06-28 07:58:49 +0200

    refactor(web-ui): remove logger in snackbar (#1511)

--- services/web-ui/src/core/snackbar/useSnackbar.ts
+++ services/web-ui/src/core/snackbar/useSnackbar.ts
@@ -1,30 +1,12 @@
 import { Grow } from "@mui/material";
 import { VariantType, enqueueSnackbar } from "notistack";
 
-import { Logger } from "src/core/logging";
 import { useIsMobile } from "src/hooks/useIsMobile";
-
-const logger = new Logger("Snackbar");
 
 export function useSnackbar() {
   const isMobile = useIsMobile();
 
   function showSnackbar(message: string, variant: VariantType): void {
-    switch (variant) {
-      case "error": {
-        logger.error(message);
-        break;
-      }
-      case "warning": {
-        logger.warn(message);
-        break;
-      }
-      default: {
-        logger.debug(message);
-        break;
-      }
-    }
-
     enqueueSnackbar(message, {
       anchorOrigin: {
         horizontal: isMobile ? "center" : "right",
