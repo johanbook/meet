b3c8f9d30fd94cc6e66b504f28285115bf891149
Johan Book <13253042+johanbook@users.noreply.github.com>
2025-06-30 10:52:45 +0200

    feat(web-ui): improve snackbar style

--- services/web-ui/src/core/snackbar/SnackbarProvider.tsx
+++ services/web-ui/src/core/snackbar/SnackbarProvider.tsx
@@ -1,7 +1,51 @@
-import { FC } from "react";
+import { FC, forwardRef } from "react";
 
-import { SnackbarProvider as NotistackSnackbarProvider } from "notistack";
+import { SnackbarContent as MuiSnackbarContent, SxProps } from "@mui/material";
+import {
+  CustomContentProps,
+  SnackbarProvider as NotistackSnackbarProvider,
+  VariantType,
+} from "notistack";
+
+const AUTO_HIDE_DURATION_MS = 3000;
+
+const getSxProps = (variant: VariantType): SxProps => {
+  if (variant === "error") {
+    return {
+      backgroundColor: "error.main",
+    };
+  }
+
+  return {};
+};
+
+const SnackbarContent = forwardRef<HTMLDivElement, CustomContentProps>(
+  (props, reference) => {
+    return (
+      <MuiSnackbarContent
+        className={props.className}
+        message={props.message}
+        ref={reference}
+        style={props.style}
+        sx={getSxProps(props.variant)}
+      />
+    );
+  },
+);
+SnackbarContent.displayName = "SnackbarContent";
 
 export const SnackbarProvider: FC = () => {
-  return <NotistackSnackbarProvider dense />;
+  return (
+    <NotistackSnackbarProvider
+      autoHideDuration={AUTO_HIDE_DURATION_MS}
+      Components={{
+        default: SnackbarContent,
+        error: SnackbarContent,
+        info: SnackbarContent,
+        success: SnackbarContent,
+        warning: SnackbarContent,
+      }}
+      dense
+    />
+  );
 };
