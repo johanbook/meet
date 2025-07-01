dd53b9b35d8fe48be20396b49a9e16d9e70fa6ea
Johan Book <13253042+johanbook@users.noreply.github.com>
2025-07-01 13:41:55 +0200

    feat(auth-ui): add custom page for password reset (#1561)

--- services/auth-ui/src/pages/ResetPassword.tsx
+++ services/auth-ui/src/pages/ResetPassword.tsx
@@ -7,55 +7,42 @@ import { Link } from "src/components/ui/Link";
 import { TextField } from "src/components/ui/TextField";
 import { useTranslation } from "src/core/i18n";
 
-import { login } from "./utils";
+import { resetPasswordByEmail } from "./utils";
 
-export const LogIn: FC = () => {
+export const ResetPassword: FC = () => {
   const { t } = useTranslation();
 
   const [isLoading, setIsLoading] = useState(false);
-
   const [email, setEmail] = useState("");
   const [emailError, setEmailError] = useState("");
-
-  const [password, setPassword] = useState("");
-  const [passwordError, setPasswordError] = useState("");
-
   const [error, setError] = useState("");
+  const [emailSent, setEmailSent] = useState(false);
 
-  const handleLogin = async (event: SyntheticEvent) => {
+  const handlePasswordReset = async (event: SyntheticEvent) => {
     event.preventDefault();
     setIsLoading(true);
+    setError("");
+    setEmailError("");
+    setEmailSent(false);
 
     try {
-      const response = await login(email, password);
+      const response = await resetPasswordByEmail(email);
 
       if (response.status === "FIELD_ERROR") {
         const field = response.formFields[0];
-
         if (field.id === "email") {
           setEmailError(field.error);
         }
-
-        if (field.id === "password") {
-          setPasswordError(field.error);
-        }
-
         return;
       }
 
-      if (response.status === "WRONG_CREDENTIALS_ERROR") {
-        setError(t("errors.wrongCredentials"));
+      if (response.status === "PASSWORD_RESET_NOT_ALLOWED") {
+        setError(t("resetPassword.notAllowed"));
         return;
       }
 
-      if (response.status === "SIGN_IN_NOT_ALLOWED") {
-        setError(response.reason);
-        return;
-      }
-
-      // Login was successful
-      const searchParams = new URLSearchParams(window.location.search);
-      window.location.href = searchParams.get("redirectToPath") || "/";
+      // Assume success if no error status
+      setEmailSent(true);
     } catch {
       setError(t("errors.generic"));
     } finally {
@@ -66,7 +53,7 @@ export const LogIn: FC = () => {
   return (
     <Box
       component="form"
-      onSubmit={handleLogin}
+      onSubmit={handlePasswordReset}
       sx={{
         display: "flex",
         flexDirection: "column",
@@ -74,11 +61,9 @@ export const LogIn: FC = () => {
         alignItems: "center",
       }}
     >
-      <Typography variant="h4">{t("login.title")}</Typography>
-
-      <Typography>
-        {t("login.notSignedUp")}
-        <Link to="/login/signup"> {t("login.signUpLink")} </Link>
+      <Typography variant="h4">{t("resetPassword.title")}</Typography>
+      <Typography color="textSecondary" gutterBottom>
+        {t("resetPassword.instructions")}
       </Typography>
 
       <TextField
@@ -93,35 +78,29 @@ export const LogIn: FC = () => {
         value={email}
       />
 
-      <TextField
-        autoComplete="current-password"
-        disabled={isLoading}
-        error={passwordError}
-        fullWidth
-        label={t("fields.password.label")}
-        name="password"
-        onChange={(value) => setPassword(value)}
-        type="password"
-        value={password}
-      />
-
       {error && (
         <Alert severity="error" sx={{ width: "100%" }}>
           {error}
         </Alert>
       )}
 
-      <Link to="/login/reset-password">Forgot password?</Link>
+      {emailSent && (
+        <Alert severity="success" sx={{ width: "100%" }}>
+          {t("resetPassword.emailSent")}
+        </Alert>
+      )}
 
       <Button
-        disabled={!email || !password}
+        disabled={!email || isLoading}
         loading={isLoading}
         fullWidth
         type="submit"
         variant="contained"
       >
-        {t("login.button")}
+        {t("resetPassword.button")}
       </Button>
+
+      <Link to="/login">{t("resetPassword.backToLogin")}</Link>
     </Box>
   );
 };
