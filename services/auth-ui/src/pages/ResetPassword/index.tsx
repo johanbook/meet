9684a3cfed539003101af7e13fa8b1da4658088f
Johan Book <13253042+johanbook@users.noreply.github.com>
2025-07-02 07:20:51 +0200

    feat(auth-ui): add not found page

--- services/auth-ui/src/pages/ResetPassword/index.tsx
+++ services/auth-ui/src/pages/ResetPassword/index.tsx
@@ -2,13 +2,14 @@ import { FC } from "react";
 
 import { getResetPasswordTokenFromURL } from "src/api/auth";
 
+import { PasswordResetFromToken } from "./PasswordResetFromToken";
 import { RequestPasswordReset } from "./RequestPasswordReset";
 
-export const PasswordReset: FC = () => {
+export const ResetPassword: FC = () => {
   const token = getResetPasswordTokenFromURL();
 
   if (token) {
-    return <p>hi</p>;
+    return <PasswordResetFromToken />;
   }
 
   return <RequestPasswordReset />;
