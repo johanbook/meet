a65fb050178bc8c3bdaad8ed0eb87101fd432999
Johan Book <13253042+johanbook@users.noreply.github.com>
2025-07-07 13:40:50 +0200

    chore(api): fix coverage pattern configuration (#1616)
    * chore(api): fix coverage patterns
    
    * chore(api): fix typo

--- services/api/vitest.config.ts
+++ services/api/vitest.config.ts
@@ -1,12 +1,29 @@
+import swc from "unplugin-swc";
 import { defineConfig } from "vitest/config";
-import swc from 'unplugin-swc';
 
 export default defineConfig({
-  plugins: [
-    swc.vite()
-  ],
+  plugins: [swc.vite()],
   test: {
     coverage: {
+      exclude: [
+        // Incorrectly configured modules should be detected
+        "**/*.module.ts",
+        // Index files only contains imports/re-exports which should be caught in linting
+        "**/index.ts",
+        // Redundant to create tests for CLI tools
+        "**/cli/**",
+        // Redundant to create tests for factories
+        "**/factories/**",
+        // Redundant to create tests for migrations
+        "**/migrations/**",
+        // Should always be ignored
+        "**/node_modules/**",
+        // Redundant to create tests for seeds
+        "**/seeds/**",
+        // No need to test testing tools
+        "**/test/**",
+      ],
+      include: ["src/**"],
       thresholds: {
         branches: 27,
         functions: 25,
@@ -14,24 +31,6 @@ export default defineConfig({
         statements: 38,
       },
     },
-    exclude: [
-      // Incorrectly configured modules should be detected
-      ".module.ts",
-      // Index files only contains imports/re-exports which should be caught in linting
-      "index.ts",
-      // Redundant to create tests for CLI tools
-      "/cli/",
-      // Redundant to create tests for factories
-      "/factories/",
-      // Redundant to create tests for migrations
-      "/migrations/",
-      // Should always be ignored
-      "/node_modules/",
-      // Redundant to create tests for seeds
-      "/seeds/",
-      // No need to test testing tools
-      "/test/",
-    ],
     globals: true,
     setupFiles: "./src/test/setup-tests",
     include: ["src/**/*.{test,spec}.{js,ts}"],
