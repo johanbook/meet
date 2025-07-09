8f3e62dc48e7bc1f47c04bfce74a79b0d9493098
Johan Book <13253042+johanbook@users.noreply.github.com>
2025-07-09 07:27:08 +0200

    fix(web-ui): hide non-featured nav items on desktop

--- services/web-ui/src/components/nav/useNavItems.ts
+++ services/web-ui/src/components/nav/useNavItems.ts
@@ -1,10 +1,34 @@
 import { useMemo } from "react";
 
 import { organizationsApi } from "src/apis";
+import { OrganizationFeature } from "src/core/organizations";
 import { CacheKeysConstants, useQuery } from "src/core/query";
 
-import { mobileNav } from "./nav.items";
-import { MobileNav } from "./types";
+import { desktopNav, mobileNav } from "./nav.items";
+import { DesktopNav, MobileNav, NavItem } from "./types";
+
+const filterByFeature = (items: NavItem[], features: OrganizationFeature[]) => {
+  return items.filter((item) =>
+    item.requiredFeatures.every((requiredFeature) =>
+      features.includes(requiredFeature),
+    ),
+  );
+};
+
+export const useDesktopNavItems = (): DesktopNav => {
+  const { data } = useQuery({
+    queryKey: [CacheKeysConstants.CurrentOrganization],
+    queryFn: () => organizationsApi.getCurrentOrganization(),
+  });
+
+  return useMemo(() => {
+    const features = data?.features || [];
+    return {
+      bottom: filterByFeature(desktopNav.bottom, features),
+      top: filterByFeature(desktopNav.top, features),
+    };
+  }, [data?.features]);
+};
 
 export const useMobileNavItems = (): MobileNav => {
   const { data } = useQuery({
@@ -15,11 +39,7 @@ export const useMobileNavItems = (): MobileNav => {
   return useMemo(() => {
     const features = data?.features || [];
     return {
-      bottom: mobileNav.bottom.filter((item) =>
-        item.requiredFeatures.every((requiredFeature) =>
-          features.includes(requiredFeature),
-        ),
-      ),
+      bottom: filterByFeature(mobileNav.bottom, features),
     };
   }, [data?.features]);
 };
