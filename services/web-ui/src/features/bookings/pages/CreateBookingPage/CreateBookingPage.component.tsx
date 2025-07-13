2c94c3d91d63de3a746612e01c22f9567536c266
Johan Book <13253042+johanbook@users.noreply.github.com>
2025-07-13 13:51:36 +0200

    feat(web-ui): improve create booking page (#1679)
    Co-authored-by: Johan Book <{ID}+{username}@users.noreply.github.com>

--- services/web-ui/src/features/bookings/pages/CreateBookingPage/CreateBookingPage.component.tsx
+++ services/web-ui/src/features/bookings/pages/CreateBookingPage/CreateBookingPage.component.tsx
@@ -1,11 +1,37 @@
 import { ReactElement } from "react";
 
-import { Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
+
+const WEEKS = [1, 2, 3, 4];
+const DAYS = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
 export function CreateBookingPageComponent(): ReactElement {
   return (
     <>
-      <Typography>You have no bookings</Typography>
+      <Typography gutterBottom variant="h5">
+        Pick a date
+      </Typography>
+
+      <Stack gap={1}>
+        {WEEKS.map((week) => (
+          <Box key={week}>
+            <Stack direction="row" gap={1}>
+              {DAYS.map((day) => (
+                <Box
+                  key={week + day}
+                  sx={{
+                    backgroundColor: "success.main",
+                    p: 1,
+                    borderRadius: 1,
+                  }}
+                >
+                  {day}
+                </Box>
+              ))}
+            </Stack>
+          </Box>
+        ))}
+      </Stack>
     </>
   );
 }
