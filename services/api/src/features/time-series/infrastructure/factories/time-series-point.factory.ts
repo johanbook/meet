a3902a7ca0de079f3d7509d0cde2909715fec475
Johan Book <{ID}+{username}@users.noreply.github.com>
2025-07-14 13:05:56 +0200

    chore(api): improve time series point factory

--- services/api/src/features/time-series/infrastructure/factories/time-series-point.factory.ts
+++ services/api/src/features/time-series/infrastructure/factories/time-series-point.factory.ts
@@ -2,13 +2,15 @@ import { setSeederFactory } from "typeorm-extension";
 
 import { TimeSeriesPoint } from "../entities/time-series-point.entity";
 
+const LABELS = ["John", "Eric", "Oscar"];
+
 export const TimeSeriesPointFactory = setSeederFactory(
   TimeSeriesPoint,
   (faker) => {
     const point = new TimeSeriesPoint();
     point.description = faker.lorem.sentence();
-    point.label = faker.color.human();
-    point.value = faker.number.int({ min: -5, max: 5 });
+    point.label = faker.helpers.arrayElement(LABELS);
+    point.value = faker.number.int({ min: -2, max: 5 });
     point.createdAt = faker.date.past();
     return point;
   },
