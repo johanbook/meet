fb9da8014c69eee7cf459124af08655ab1c3fba4
Johan Book <{ID}+{username}@users.noreply.github.com>
2025-07-14 20:11:10 +0200

    feat(web-ui): move time series chart into accordion

--- services/web-ui/src/features/time-series/pages/TimeSeriesPage/TimeSeriesPage.nav.tsx
+++ services/web-ui/src/features/time-series/pages/TimeSeriesPage/TimeSeriesPage.nav.tsx
@@ -5,15 +5,17 @@ import { useTranslation } from "src/core/i18n";
 
 interface TimeSeriesPageNavProps {
   children: ReactNode;
+  title?: string;
 }
 
 export function TimeSeriesPageNav({
   children,
+  title,
 }: TimeSeriesPageNavProps): ReactElement {
   const { t } = useTranslation("time-series");
 
   return (
-    <Nav navBackTo="/time-series" padding="normal" title={t("header")}>
+    <Nav navBackTo="/time-series" padding="normal" title={title || t("header")}>
       {children}
     </Nav>
   );
