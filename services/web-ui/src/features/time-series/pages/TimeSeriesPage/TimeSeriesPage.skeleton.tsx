a529c51c2d3c29c723a27d16f85e1ba0a37f2961
Johan Book <{ID}+{username}@users.noreply.github.com>
2025-07-24 09:20:54 +0200

    feat(web-ui): improve loading state for time series page

--- services/web-ui/src/features/time-series/pages/TimeSeriesPage/TimeSeriesPage.skeleton.tsx
+++ services/web-ui/src/features/time-series/pages/TimeSeriesPage/TimeSeriesPage.skeleton.tsx
@@ -1,9 +1,30 @@
 import { FC } from "react";
 
-import { Box, Skeleton } from "@mui/material";
+import { Box, Skeleton, Stack } from "@mui/material";
 
 export const TimeSeriesPageSkeleton: FC = () => (
   <Box>
-    <Skeleton variant="rectangular" height={140} />
+    <Skeleton variant="rectangular" height={25} />
+
+    <Stack direction="row" gap={2} sx={{ mt: 2 }}>
+      <Skeleton variant="rectangular" height={112} width={164} />
+      <Skeleton variant="rectangular" height={112} width={164} />
+    </Stack>
+
+    <Skeleton
+      variant="rectangular"
+      height={75}
+      sx={{ mt: 2, borderRadius: 2 }}
+    />
+    <Skeleton
+      variant="rectangular"
+      height={500}
+      sx={{ mt: 2, borderRadius: 2 }}
+    />
+    <Skeleton
+      variant="rectangular"
+      height={75}
+      sx={{ mt: 2, borderRadius: 2 }}
+    />
   </Box>
 );
