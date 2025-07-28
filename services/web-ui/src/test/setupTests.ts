724fcd8a1b899092b06975e2d0959cc5c9ed620c
Johan Book <{ID}+{username}@users.noreply.github.com>
2025-07-28 11:13:31 +0200

    test(web-ui): remove test mocks no longer needed

--- services/web-ui/src/test/setupTests.ts
+++ services/web-ui/src/test/setupTests.ts
@@ -3,15 +3,5 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
-import { TextDecoder, TextEncoder } from "node:util";
 
 process.env.TZ = "UTC";
-
-if (!global.TextEncoder) {
-  global.TextEncoder = TextEncoder;
-}
-
-if (!global.TextDecoder) {
-  // @ts-expect-error Needed for tests in react-router@7
-  global.TextDecoder = TextDecoder;
-}
