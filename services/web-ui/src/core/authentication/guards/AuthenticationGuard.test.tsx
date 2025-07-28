09026f4a24a2f13027f186bbc7ed2fad06fc594b
Johan Book <13253042+johanbook@users.noreply.github.com>
2025-07-28 12:47:53 +0200

    test(web-ui): fix skipped authentication test (#1781)
    Co-authored-by: Johan Book <{ID}+{username}@users.noreply.github.com>

--- services/web-ui/src/core/authentication/guards/AuthenticationGuard.test.tsx
+++ services/web-ui/src/core/authentication/guards/AuthenticationGuard.test.tsx
@@ -1,16 +1,27 @@
+import { profileApi } from "src/apis";
 import {
   ReactQueryTestProvider,
+  afterEach,
   describe,
   expect,
   it,
   render,
   screen,
+  vi,
 } from "src/test";
 
 import { AuthenticationGuard } from ".";
 
-describe.skip("<AuthenticationGuard />", () => {
-  it("renders a message", async () => {
+describe("<AuthenticationGuard />", () => {
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  it("renders children if authentication is successful", async () => {
+    vi.spyOn(profileApi, "checkIfProfileExists").mockImplementation(
+      async () => true,
+    );
+
     render(
       <ReactQueryTestProvider>
         <AuthenticationGuard>
@@ -19,7 +30,26 @@ describe.skip("<AuthenticationGuard />", () => {
       </ReactQueryTestProvider>,
     );
 
-    const message = await screen.findByText(/Welcome/);
+    const message = await screen.findByText(/my-text/);
+    expect(message).toBeInTheDocument();
+  });
+
+  it("does not render children if authentication fails", async () => {
+    vi.spyOn(profileApi, "checkIfProfileExists").mockImplementation(
+      async () => {
+        throw new Error("my-error");
+      },
+    );
+
+    render(
+      <ReactQueryTestProvider>
+        <AuthenticationGuard>
+          <p>my-text</p>
+        </AuthenticationGuard>
+      </ReactQueryTestProvider>,
+    );
+
+    const message = await screen.findByText(/Unable to verify login/);
     expect(message).toBeInTheDocument();
   });
 });
