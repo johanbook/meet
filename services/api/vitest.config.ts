8ddc31fcdf0ea5170dfe047486f7f542468754a5
Johan Book <13253042+johanbook@users.noreply.github.com>
2026-06-09 12:15:42 +0200

    chore: fix resolve path

--- services/api/vitest.config.ts
+++ services/api/vitest.config.ts
@@ -1,8 +1,15 @@
+import { resolve } from "node:path";
+
 import swc from "unplugin-swc";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [swc.vite()],
+  resolve: {
+    alias: {
+      src: resolve(__dirname, "src"),
+    },
+  },
   test: {
     coverage: {
       exclude: [
