ab3b7dd1b2906924aecbbf32e32cd38116a052cf
Johan Book <13253042+johanbook@users.noreply.github.com>
2026-07-21 13:12:20 +0200

    chore: add build\

--- services/auth-ui/vite.config.ts
+++ services/auth-ui/vite.config.ts
@@ -1,10 +1,10 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { fileURLToPath, URL } from "node:url";
-import { getBareModules as getEmailPasswordBareModules } from "supertokens-auth-react/recipe/emailpassword/utils";
-import { getBareModules as getEmailVerificationBareModules } from "supertokens-auth-react/recipe/emailverification/utils";
-import { getBareModules as getSessionBareModules } from "supertokens-auth-react/recipe/session/utils";
-import { getBareModules as getCoreBareModules } from "supertokens-auth-react/utils";
+import { getBareModules as getEmailPasswordBareModules } from "supertokens-auth-react/lib/build/recipe/emailpassword/utils";
+import { getBareModules as getEmailVerificationBareModules } from "supertokens-auth-react/lib/build/recipe/emailverification/utils";
+import { getBareModules as getSessionBareModules } from "supertokens-auth-react/lib/build/recipe/session/utils";
+import { getBareModules as getCoreBareModules } from "supertokens-auth-react/lib/build/utils";
 
 export default defineConfig({
   base: "/login",
